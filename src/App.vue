7798cc2a4ad80c4117d8e0a1397bef32f4a0bf2c
Elyor Ravshanov <elyor1996ravshanov@gmail.com>
2022-08-19 18:02:29 +0500

    123

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <p> count: <strong> {{ count }} </strong></p>
+    <button>{{ Increment }}</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      count: 0,
+    }
+  }
+}
+
+</script>
+
+<style>
+
+</style>
