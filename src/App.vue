15ea5752f9e9e831548364d42a189b33406d061e
Elyor Ravshanov <elyor1996ravshanov@gmail.com>
2022-08-22 19:20:19 +0500

    changes

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,25 @@
 <template>
   <div>
     <p> count: <strong> {{ count }} </strong></p>
-    <button>{{ Increment }}</button>
+    <button class="btn btn-success" v-on:click="increment">Increment</button>
+    <button class="btn btn-primary" v-on:click="decrement">Deccrement</button>
+  </div>
+  <div>
+    <div class="comment">
+      <div><b>Name:</b> Samar Komilov </div>
+      <div><b>Email:</b> samarkomilov@gmail.com </div>
+      <div><b>Content:</b> Learn programming </div>
+    </div>
+    <div class="comment">
+      <div><b>Name:</b> Samar Komilov </div>
+      <div><b>Email:</b> samarkomilov@gmail.com </div>
+      <div><b>Content:</b> Learn programming </div>
+    </div>
+    <div class="comment">
+      <div><b>Name:</b> Samar Komilov </div>
+      <div><b>Email:</b> samarkomilov@gmail.com </div>
+      <div><b>Content:</b> Learn programming </div>
+    </div>
   </div>
 </template>
 
@@ -10,12 +28,32 @@ export default {
   data(){
     return{
       count: 0,
-    }
+
+      comments: [
+        {id: 1, name: "Elyor", email: "elyor@gmail.com", content: "Learning programming"},
+        {id: 1, name: "Elyor", email: "elyor@gmail.com", content: "Learning programming"},
+        {id: 1, name: "Elyor", email: "elyor@gmail.com", content: "Learning programming"},
+      ]
+      }
+    },
+  methods:{
+    increment(){
+      this.count +=1
+    },
+    decrement(){
+      this.count -=1
+    },
+
   }
 }
 
 </script>
 
 <style>
+  .comment{
+    border: 3px solid #999;
+    padding: 10px;
+    margin: 10px;
 
+  }
 </style>
